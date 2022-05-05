9f76a7bd5e1fd449a35b734b41786db859c77076
Vikelle <virginie.kellermann@gmail.com>
2022-05-05 15:40:16 +0200

    toto

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 let forbiddenWord = [
     ["connard", "Que veux tu exprimer"],
-    ["Salope", "Pourquoi ?"],
-    ["Con", "Sais tu que con désigne l'appareil génital féminin, ce n'est pas une insulte"],
+    ["salope", "Pourquoi ?"],
+    ["blabla", "Sais tu que con désigne l'appareil génital féminin, ce n'est pas une insulte"],
 ]
 
 function researchWord () {
@@ -9,19 +9,23 @@ function researchWord () {
     let arrayString = bodyText.split(' ');  // Créer un tableau de CHAQUE MOT de bodyText
     console.log(arrayString);
   
-// for (let i=0; i<arrayString
-   if (arrayString.includes(forbiddenWord[0][0])) {
+for (let i=0; i<forbiddenWord.length; i++) {
+    if (arrayString.includes(forbiddenWord[i][0])) {
         //console.log(forbiddenWord[0][0]);
-        alert("Nous avons détecté le mot " + forbiddenWord[0][0]+ '\n' + forbiddenWord[0][1]);
-        return true
+        alert("Nous avons détecté le mot " + forbiddenWord[i][0]+ '\n' + forbiddenWord[i][1]);
+        //return true
     }
-    else {
-        return false
-    }
+    //else {
+    //    return false
+    //}
+}
+
+
+   
 }//console.log(researchWord ());
 //if (researchWord () = true) {
 
-researchWord();
+//researchWord();
 
 
 //}
@@ -29,7 +33,61 @@ let changeColor ='<span style="color:red">' + forbiddenWord[0][0] + '</span>'
 document.body.innerHTML=document.body.innerHTML.replaceAll(forbiddenWord[0][0], changeColor);
 //document.body.innerHTML = document.body.innerHTML.replaceAll('forbiddenWord', '<span style="color:red">interdit</span>');
 
+const bodyText = document.body.innerText;
+let arrayString = bodyText.split(' ');  // Créer un tableau de CHAQUE MOT de bodyText
+    console.log(arrayString);
 
+function researchWord2() {
+    var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            for (let i=0; i<forbiddenWord.length; i++) {
+            //var replacedText = text.replace(forbiddenWord[i][0], '[new word or phrase]');
+            if (arrayString.includes(forbiddenWord[i][0])) {
+                //console.log(forbiddenWord[0][0]);
+                alert("Nous avons détecté le mot " + forbiddenWord[i][0]+ '\n' + forbiddenWord[i][1]);
+            }
+            }
+            //if (replacedText !== text) {
+            //    element.replaceChild(document.createTextNode(replacedText), node);
+            //}
+    }
+}
+}
+}
+
+researchWord2();
+
+
+function researchWord3(){
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace('le', 'Virginie');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
+}
+
+//researchWord3();
 
 // document.getElementById('test').innerHTML = forbiddenWord[0][0] + '<br>';
 // document.getElementById('test').innerHTML  += forbiddenWord[0][1];
