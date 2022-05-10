2dda637cd8d89f3a3fd533cd0f93946ef17c9cad
Marine <marine.marez@gmail.com>
2022-05-10 17:02:39 +0200

    notre code fonctionne ! reste à lier l'API et voir comment contourner les div des gros sites

--- js/search.js
+++ js/search.js
@@ -1,12 +1,12 @@
 let forbiddenWord = ["connard", "salope", "blabla"]
-
 let fields = document.querySelectorAll('input', 'textarea');
 //console.log(fields[0].value);
 
 // Pour tous les éléments dans 'fields', diviser la valeur des éléments en mots puis détecter les mots inclus dans le tableau 'forbiddenWord'
-function researchWord () {
+function researchWord (event) {
+    //console.log(event);
     for (let j of fields) { // Ici J est un élément (la valeur) du tableau field
-        console.log(j);
+        //console.log(j);
         let arrayString = j.value.split(' ');  // Créer un tableau de CHAQUE MOT de bodyText
         //console.log(arrayString);
         // OU
@@ -15,13 +15,22 @@ function researchWord () {
 
         for (let i=0; i<forbiddenWord.length; i++) {
             if (arrayString.includes(forbiddenWord[i])) {
-            //console.log(forbiddenWord[0][0]);
                 alert("Nous avons détecté le mot " + forbiddenWord[i] + "\n" + "Es-tu bien sûr.e de vouloir envoyer ce message ?");
             }
         }
     }
 }
 
-document.getElementById('search').addEventListener('submit', researchWord());
+document.addEventListener('submit', researchWord);
 
 
+/*document.addEventListener('submit', () => {
+    for (let j of fields) { // Ici J est un élément (la valeur) du tableau field
+        let arrayString = j.value.split(' '); 
+        for (let i=0; i<forbiddenWord.length; i++) {
+            if (arrayString.includes(forbiddenWord[i])) {
+                alert("Nous avons détecté le mot " + forbiddenWord[i] + "\n" + "Es-tu bien sûr.e de vouloir envoyer ce message ?");
+            }
+        }
+    }
+});*/
