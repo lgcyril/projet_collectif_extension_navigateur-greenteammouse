7c9af409a9c82a38c6e36605a359cf533ac403ed
Marine <marine.marez@gmail.com>
2022-05-12 12:49:13 +0200

    dernière version avant démo

--- js/search.js
+++ js/search.js
@@ -1,82 +1,63 @@
-let forbiddenWord = ["connard", "salope", "blabla"]
-let fields = document.querySelectorAll('input', 'textarea');
-let j;
-//console.log(fields[0].value);
-
 // Pour tous les éléments dans 'fields', diviser la valeur des éléments en mots puis détecter les mots inclus dans le tableau 'forbiddenWord'
 function researchWord (event) {
-    event.preventDefault();
-    //console.log(event);
-    for (j of fields) { // Ici J est un élément (la valeur) du tableau field
-        //console.log(j);
-        let arrayString = j.value.split(' ');  // Créer un tableau de CHAQUE MOT de bodyText
-        //console.log(arrayString);
-        // OU
-        //for (let j in fields) { => Ici J est un indice
-        //let arrayString = fields[j].value.split(' ');  //
-
-        for (let i=0; i<forbiddenWord.length; i++) {
-            if (arrayString.includes(forbiddenWord[i])) {
-                let answer = confirm("Nous avons détecté le mot " + forbiddenWord[i] + "\n" + "Es-tu bien sûr.e de vouloir envoyer ce message ?");
-               if(answer === true){
-                // Code à éxécuter si le l'utilisateur clique sur "OK"    
-                document.querySelector('input', 'textarea').submit;
-               } 
-            }
-        }
-    }
-}
+  let fields = document.querySelectorAll('input', 'textarea');
+  let j;
+  event.preventDefault();
+  for (j of fields) { // Ici J est un élément (HTML ou balise) du tableau field
+      //let arrayString = j.value.split(' ');  // Créer un tableau de CHAQUE MOT de bodyText
+      // OU
+      //for (let j in fields) { => Ici J est un indice
+      //let arrayString = fields[j].value.split(' ');
+      console.log("Dans for j, valeur de submit : " + document.querySelector('input', 'textarea').submit);
+      // API :
+      let cache = {};
+      const request = (url, params = {}, method = "POST") => {
+      let cacheKey = JSON.stringify({ url, params, method });
+      if (cache[cacheKey]) {
+          console.log("Dans if cache, valeur de cache : " + cache[cacheKey]);
+          return cache[cacheKey];
+      }
+      let options = {
+          method
+      };
+      if ("POST" === method) {
+          url += "?" + new URLSearchParams(params).toString();
+          console.log(url)
+      } else {
+          options.body = JSON.stringify(params);
+      }
+      const result = fetch(url, options).then((response) => response.json());
+      cache[cacheKey] = result;
+      return result;
+      };
+      const get = (url, params) => request(url, params, "GET");
+      const post = (url, params) => request(url, params, "POST");
+      return post("https://api.sightengine.com/1.0/text/check.json",{
+          'text': j.value,  // liste des mots tapés dans le formulaire
+          'mode': 'standard',
+          'lang': 'fr',
+          'opt_countries': 'us,gb,fr',
+          'models': 'nudity,wad,offensive,text-content,gore,text',
+          'api_user': '1817344764',
+          'api_secret': 'uZ2h84C8TtHs3nkhrbE6',
+      }).then((post) => {   // la promesse peut mettre un temps a executer mais le formulaire est envoye
+          console.log(post);
+          console.log(post.status);
+         // console.log(post.profanity.matches[0].match);  // affiche le mot ijurieux
+         console.log(post.profanity.matches.length);
+          if (post.profanity.matches.length != 0) { // il y a une insulte!
+              let answer = confirm("Nous avons détecté le mot " + post.profanity.matches[0].match + "\n" + "Es-tu bien sûr.e de vouloir envoyer ce message ?");
+              console.log("Dans if, valeur de answer : " + answer);
+              if(answer === true){  // clique OK
+                  // Code à éxécuter si le l'utilisateur clique sur "OK"
+                  console.log("Dans if TRUE, valeur de submit : " + document.querySelector('#search').submit());
+                  document.getElementById('search').submit();  // envoie la recherche
+              } else {
+                 //event.stopPropagation();  // sinon ca bloque l'envoi preventDefault
+              }  // fin else
+          }  // fin if
+      }); // fin then((post)
+  } // fin for
+}  // fin fonction
 
 document.addEventListener('submit', researchWord);
-
-// API
-/*let cache = {};
-const request = (url, params = {}, method = "POST") => {
-  let cacheKey = JSON.stringify({ url, params, method });
-  if (cache[cacheKey]) {
-    return cache[cacheKey];
-  }
-  let options = {
-    method
-  };
-  if ("POST" === method) {
-    url += "?" + new URLSearchParams(params).toString();
-    console.log(url)
-  } else {
-    options.body = JSON.stringify(params);
-  }
-  const result = fetch(url, options).then((response) => response.json());
-  cache[cacheKey] = result;
-  return result;
-};
-const get = (url, params) => request(url, params, "GET");
-const post = (url, params) => request(url, params, "POST");
-// Test.
-post("https://api.sightengine.com/1.0/text/check.json",{
-    'text': j,
-    'mode': 'standard',
-    'lang': 'fr',
-    'opt_countries': 'us,gb,fr',
-    'models': 'nudity,wad,offensive,text-content,gore,text',
-    'api_user': '1817344764',
-    'api_secret': 'uZ2h84C8TtHs3nkhrbE6',
-}).then((post) => {
-  //document.getElementById("app").innerHTML += 'text : putain ça marche!<br>models : nudity,wad,offensive,text-content,gore,text<br>'
-  //+ post.status;
-  console.log(post);
-  console.log(post.status);
-
-});*/
-
-
-
-/*document.addEventListener('submit', () => {
-    for (let j of fields) { // Ici J est un élément (la valeur) du tableau field
-        let arrayString = j.value.split(' '); 
-        for (let i=0; i<forbiddenWord.length; i++) {
-            if (arrayString.includes(forbiddenWord[i])) {
-                alert("Nous avons détecté le mot " + forbiddenWord[i] + "\n" + "Es-tu bien sûr.e de vouloir envoyer ce message ?");
-            }
-        }
-    }
-});*/
